9d296532120cacae5643546734f8ba2b23a4bb80
Halil Burak Olgun <halilburak2002@gmail.com>
2024-11-02 12:05:57 +0300

    first

---
+++ Climassist_Last/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Climassist_Last.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
